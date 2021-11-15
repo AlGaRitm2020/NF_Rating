496f5512bb6af26da47028df15ecdf98e7a46082
Albert <62512656+AlGaRitm2020@users.noreply.github.com>
2021-11-15 13:44:55 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,2 @@
+TEST_TOKEN = '2113371867:AAGSIvzB0u83lGE1va3QHsxNvgwEcHZLqwA'
+DATABASE_URL = 'postgres://tbfchzunvfsnzi:e8f9ba6b0e6cb8b56a21c8817e7403a9d90929c1b9eb4941c01d212f0ac9b367@ec2-54-155-61-133.eu-west-1.compute.amazonaws.com:5432/d1d8hutvqu5m8c'
